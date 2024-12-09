9cb60d06e847b3eb3f16f86f45df242fd018c355
Emma Blanchard <emma.blnch4@gmail.com>
2024-12-09 08:46:16 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -16,7 +16,7 @@ static int	check_errors(int fd, char **stored_lines)
 {
 	char	temp[1];
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, temp, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	if (!*stored_lines)
 		*stored_lines = allocate_string(0);
